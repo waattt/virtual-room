7eda5b3627eff6b11e29d28f68cff67feafb77d9
Timon van Dreumel <satesous@gmail.com>
2015-12-01 15:25:15 +0100

    connecting to server and finding specific beacons

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
+// Requirements!
 var noble = require('noble');
 
-//replace localhost with your server's IP;
-var socket = require('socket.io-client')('http://192.168.0.136/scanner');
+// IMPORTANT
+// Change server ip!
+var server = 'http://192.168.0.136:80';
+var socket = require('socket.io-client')(server);
 
-//replace with your hardware address
-var addressToTrack = 'd4f5137787f5'; 
+console.log('Connecting to ' + server + '....');
+
+// MAC Adressen van de iBeacons
+var beacon1 = 'd4f5137787f5',
+    beacon2 = '',
+    beacon3 = '',
+    beacon4 = '',
+    beacon5 = '';
 
 socket.on('connect', function(){  
-    console.log('connected to server');
+    console.log('connected to server');    
 });
 
 noble.on('discover', function(peripheral){
-    if(peripheral.uuid == addressToTrack){
+    if(peripheral.uuid == beacon1 ||
+       peripheral.uuid == beacon2 ||
+       peripheral.uuid == beacon3 ||
+       peripheral.uuid == beacon4 ||
+       peripheral.uuid == beacon5 ){
+        console.log('specific device found' + peripheral.rssi);
         socket.emit('deviceData', {mac: peripheral.uuid, rssi:peripheral.rssi});    
     }
 });
+
 noble.on('stateChange', function(state) {
     if (state === 'poweredOn') {
         noble.startScanning([], true);
