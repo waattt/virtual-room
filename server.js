a259f2e2acae632ab1271637703fa7588b3bb21d
waattt <timonvandreumel@gmail.com>
2015-12-02 14:24:24 +0100

    Trying to make it visual

--- server.js
+++ server.js
@@ -21,12 +21,39 @@ function handler (req, res) {
 var scanner = io.of('/scanner');
 scanner.on('connection', function(socket){
     console.log('Scanner Connected');
-    
-    socket.on('deviceData', function(data) {
-        console.log(data.rssi);
-    });
 
     socket.on('disconnect', function() {
         console.log('Scanner Disconnected');
     });
+
+    socket.on('deviceData', function(data) {
+        console.log(data.rssi);
+        io.of('/client').emit('message', calculateDistance(data.rssi));
+    });
 });
+var client = io.of('/client');
+client.on('connection', function(socket){
+    console.log('Client Connected');
+
+    socket.on('disconnect', function() {
+        console.log('Client Disconnected');
+    });
+});
+///////////////////////////////////////////////////////////////////////
+/////// Calculate the distance
+function calculateDistance(rssi) {
+  var txPower = -59;
+
+  if (rssi == 0) {
+    return -1.0;
+  }
+
+  var ratio = rssi*1.0/txPower;
+  if (ratio < 1.0) {
+    return Math.pow(ratio,10);
+  }
+  else {
+    var distance =  (0.89976)*Math.pow(ratio,7.7095) + 0.111;
+    return distance;
+  }
+}
